12fed04bc37a5bfddd8e526d3c7e1a378b3a1d6b
Swift Funcional <info@swiftfuncional.com>
2017-05-21 20:50:28 +0200

    But, we would want to parse different formats: JSON, XML, HTML

--- Funciones como ciudadanos de primera clase.playground/Contents.swift
+++ Funciones como ciudadanos de primera clase.playground/Contents.swift
@@ -1,10 +1,18 @@
 import Foundation
 
 // (String) -> [String: AnyObject]
-func parse(_ response: String) -> [String: AnyObject] {
+func parseAsJSON(_ response: String) -> [String: AnyObject] {
 	let data = response.data(using: .utf8)!
 
 	return try! JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
 }
 
-parse("{\"data\": 42}")
+func parseAsXML(_ response: String) -> [String: AnyObject] {
+	return [:]
+}
+
+func parseAsHTML(_ response: String) -> [String: AnyObject] {
+	return [:]
+}
+
+parseAsJSON("{\"data\": 42}")
